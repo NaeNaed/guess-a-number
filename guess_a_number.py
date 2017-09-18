933eb7980803374ba1e0fa4e6fc2de0f7955b1c3
Huavil <ahuavi3066@greenville.k12.sc.us>
2017-09-18 14:34:14 -0400

    Added a loop and play again feature

--- guess_a_number.py
+++ guess_a_number.py
@@ -14,30 +14,50 @@ def get_guess():
             guess = int(guess)
             return guess
         else:
-            print("Please enter a number that is positive.")
-                  
-#start game
-rand = random.randint(low, high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
+            print("Please enter a number that is positive")
 
-guess = -1
-tries = 0
+def play_again():
+    while True:
+        decision = input("Do you want to play again? y/n ")
 
-#play game
-while guess != rand and tries < limit:
-    guess = get_guess()
-    
-    if guess < rand:
-        print("You guessed too low.")
-    elif guess > rand:
-        print("You guessed too high.")
+        if decision == 'y' or decision == 'yes':
+            return True
+        elif decision == 'n' or decision == 'no':
+            return False
+        else:
+            print("Just type yes or no already")
+
+again = True
+
+while again:
+    #start game
+    rand = random.randint(low, high)
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
+
+    guess = -1
+    tries = 0
+
+    #play game
+    while guess != rand and tries < limit:
+        guess = get_guess()
         
-    tries += 1
+        if guess < rand:
+            print("You guessed too low.")
+        elif guess > rand:
+            print("You guessed too high.")
+            
+        tries += 1
 
-#end game
-if guess == rand:
-    print("You win!")
-else:
-    print("NANI!? You have no tries left!")
+    #end game
+    if guess == rand:
+        print("You win!")
+    else:
+        print("NANI!? You have no tries left!")
+
+    again = play_again()
+
+
+print("Goodbye.")
+
 
     
